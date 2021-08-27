0367f33fa14b9698a31acb023e488e1df80859c0
SHUSSAIN-accenture <shakahwath.hussain@accenture.com>
2021-08-27 16:35:37 +0100

    Second upload

--- flowchartq2.java
+++ flowchartq2.java
@@ -1,5 +1,33 @@
 package activityjava;
 
 public class flowchartq2 {
-
+	//Flow Chart   
+	public static void main(String[] args) {
+	    flowchart(7000);
+	   
+	}
+	
+	private static void flowchart(int A) {
+	    // TODO Auto-generated method stub
+	   
+	    if (A > 2000) {
+	        System.out.println("A");
+	        if (A > 6000) {
+	            System.out.println("C");
+	        } else {
+	            System.out.println("B");
+	        }
+	    } else {
+	        System.out.println("1");
+	       
+	        if (A > 100) {
+	            System.out.println("2");
+	
+	        } else {
+	            System.out.println("3");
+	       
+	    }
+	    }
+	}
+	
 }
